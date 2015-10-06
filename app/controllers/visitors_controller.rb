0ca601ca10a2f3611730845eadfc2c8fcbf6df6f
roberm <roberm@users.noreply.github.com>
2015-10-06 16:34:57 +0200

    mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
 
-	def new
+	def new_old
 		@owner = Owner.new
 
 		# Flash message
@@ -26,6 +26,32 @@ class VisitorsController < ApplicationController
 
 		# Custom layout
 		#render 'visitors/new', :layout => 'cutomlay'
+	end
+
+	def new
+		@visitor = Visitor.new
+	end
+
+	def create
+
+		@visitor = Visitor.new(secure_params)
+
+		if @visitor.valid?
+
+			@visitor.subscribe
+
+			flash[:notice] = "Signed up #{@visitor.email}"
+
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def secure_params
+		params.require(:visitor).permit(:email)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,8 @@ Rails.application.routes.draw do
 
   resources :contacts, only: [ :new, :create ]
 
+  resources :visitors, only: [ :new, :create ]
+
   root to: 'visitors#new'
 
 end
